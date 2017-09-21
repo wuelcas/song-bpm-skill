b13c4a74a12896a5fa8cda28f94302f5fbab4016
Wuelber Castillo <wuelber.castillo@gmail.com>
2017-09-21 10:42:15 -0600

    Fix eslint errors and delete zombie code

--- config/index.js
+++ config/index.js
@@ -6,12 +6,6 @@ const env = require('./env').toLowerCase();
 const configFile = require(path.join(__dirname, `${env}.json`));
 configFile.env = env;
 
-/*******  plugins  *******/
-
-
-/*****  plugins end *****/
-
-
 
 module.exports = configFile;
 module.exports.asFunction = () => configFile;
